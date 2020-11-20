9061aff1136f9b41345b97f225bc240296d2f3b8
Emily <linemily97@gmail.com>
2020-11-20 00:48:47 -0800

    songs redux practice

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 const songsReducer = () => {
   return [
     { title: 'No Scrubs', duration: '4:05' },
@@ -6,3 +8,18 @@ const songsReducer = () => {
     { title: 'I want it that way', duration: '1:20' },
   ];
 };
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+//KEYS HERE WILL BE IN THE STATE OBJECT
+//I.E. STATE.SELECTEDSONG
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
